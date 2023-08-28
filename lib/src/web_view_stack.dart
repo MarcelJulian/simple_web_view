70c8a5b4720df2f470309b0cda93d28a0787daf2
MarcelJulian <marceljulian2047@gmail.com>
2023-08-28 21:17:17 +0700

    fix: :fire: Remove pull to refresh

--- lib/src/web_view_stack.dart
+++ lib/src/web_view_stack.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:simple_web_view/src/pull_to_refresh.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewStack extends StatefulWidget {
@@ -13,56 +11,18 @@ class WebViewStack extends StatefulWidget {
 
 class _WebViewStackState extends State<WebViewStack> {
   var loadingPercentage = 0;
-  late DragGesturePullToRefresh dragGesturePullToRefresh;
+  // late DragGesturePullToRefresh dragGesturePullToRefresh;
 
   @override
   void initState() {
     super.initState();
-
-    dragGesturePullToRefresh = DragGesturePullToRefresh();
-
-    widget.controller.setNavigationDelegate(NavigationDelegate(
-      onPageStarted: (url) {
-        dragGesturePullToRefresh.started();
-        setState(() {
-          loadingPercentage = 0;
-        });
-      },
-      onProgress: (progress) {
-        setState(() {
-          loadingPercentage = progress;
-        });
-      },
-      onPageFinished: (url) {
-        dragGesturePullToRefresh.finished();
-        setState(() {
-          loadingPercentage = 100;
-        });
-      },
-    )
-        // ..loadRequest(
-        //   Uri.parse('https://dashboard.siapjadiasn.com/'),
-        // )
-        // ..setJavaScriptMode(JavaScriptMode.unrestricted);
-        );
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        RefreshIndicator(
-          onRefresh: () => dragGesturePullToRefresh.refresh(),
-          child: Builder(builder: (contextRefresh) {
-            dragGesturePullToRefresh
-                .setContext(contextRefresh)
-                .setController(widget.controller);
-
-            return WebViewWidget(
-                controller: widget.controller,
-                gestureRecognizers: {Factory(() => dragGesturePullToRefresh)});
-          }),
-        ),
+        WebViewWidget(controller: widget.controller),
         if (loadingPercentage < 100)
           LinearProgressIndicator(
             value: loadingPercentage / 100.0,
